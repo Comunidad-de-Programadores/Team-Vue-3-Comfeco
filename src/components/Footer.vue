b5ec12f0818071b6cbbc342ea3c855fa973b1278
Joaquin Martinez <joaquinjo20009@gmail.com>
2021-02-16 18:20:55 -0300

    correccion de rutas en componente Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,56 +1,101 @@
 <template>
-    <div>
-        <footer class="relative w-full h-auto py-px-50 pr-px-100 flex space-between flex-wrap">
-            <div class="flex space-between flex-wrap flex-row">
-                <div class="ml-8 mt-5 mr-8 inline-block">
-                     <h2 class="relative font-medium mb-1 border-b-2 max-w-max border-primary">
-                        About Us
-                    </h2>
-                    <p class=" w-50">
-                        Participa del primer festival de código en hispanoamérica hecho por desarrolladores, para desarrolladores!
-                    </p>
-                    </div>
-                    <div class="ml-8 mt-5 mr-8 inline-block">
-                    <ul>
-                        <h2 class="relative font-medium mb-1 border-b-2 border-black max-w-max border-primary">Follow Us</h2>
-                        <li class="flex mt-5">
-                            <a class="flex inline-block w-5 h-2 content-center items-center m-2" href="https://www.youtube.com/channel/UC0oi8uH1vxDcyt7b_3iByew" target="_blank" rel="noopener noreferrer">
-                            <img src="../assets/icons/Youtube.svg" alt="Youtube Logo">
-                            </a>
-                            <a class="flex inline-block w-5 h-2 content-center items-center m-2" href="https://www.facebook.com/groups/2637132626546045" target="_blank" rel="noopener noreferrer">
-                            <img src="..\assets\icons\Facebook.svg" alt="Facebook Logo">
-                            </a>
-                            <a class="flex inline-block w-5 h-2 content-center items-center m-2" href="https://www.instagram.com/comfeco/" target="_blank" rel="noopener noreferrer">
-                                <img src="..\assets\icons\Instagram.svg" alt="Instagram Logo">
-                            </a>
-                            <a class="flex inline-block w-5 h-2 content-center items-center m-2" href="https://discord.com/invite/5Bb5yvzNPr" target="_blank" rel="noopener noreferrer">
-                                <img src="..\assets\icons\discord.svg" alt="Discord Logo">
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="ml-8 mt-3 mb-3 mr-8 inline-block">
-                    <h2 class="relative font-medium mb-1 border-b-2 border-black max-w-max border-primary">Quick Links</h2>
-                    <ul>
-                        <li class="flex mt-1"><router-link to="/Terminos-y-Condiciones" target="_blank">Privacity Policy</router-link></li>
-                        <li class="flex mt-1"><a href="/Terminos-y-condiciones" target="_blank">Terms & Conditions</a></li>
-                        <li class="flex mt-1"><a href="https://discord.com/invite/5Bb5yvzNPr" target="_blank">Contac Us</a></li>
-                    </ul>
-                </div>
-                <div class=" w-96">
-                    <div class="inline-block">
-                        <img class="w-10 h-10 ml-8 inline-block" src="..\assets\images\Isotipo-brand.png" alt="COMFECO Logo">
-                        <p class="w-max-w ml-8 inline-block">Comunity Fest and Code</p>
-                    </div>
-                    <p class="ml-32 sm:ml-32 text-center inline-block">COMFECO &copy; 2020</p>
-                </div>
-            </div>
-        </footer>
-    </div>
+  <div>
+    <footer
+      class="relative w-full h-auto py-px-50 pr-px-100 flex space-between flex-wrap"
+    >
+      <div class="flex space-between flex-wrap flex-row">
+        <div class="ml-8 mt-5 mr-8 inline-block">
+          <h2 class="relative font-medium mb-1 border-b-2 max-w-max border-primary">
+            About Us
+          </h2>
+          <p class="w-50">
+            Participa del primer festival de código en hispanoamérica hecho por
+            desarrolladores, para desarrolladores!
+          </p>
+        </div>
+        <div class="ml-8 mt-5 mr-8 inline-block">
+          <ul>
+            <h2
+              class="relative font-medium mb-1 border-b-2 border-black max-w-max border-primary"
+            >
+              Follow Us
+            </h2>
+            <li class="flex mt-5">
+              <a
+                class="flex inline-block w-5 h-2 content-center items-center m-2"
+                href="https://www.youtube.com/channel/UC0oi8uH1vxDcyt7b_3iByew"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="../assets/icons/Youtube.svg" alt="Youtube Logo" />
+              </a>
+              <a
+                class="flex inline-block w-5 h-2 content-center items-center m-2"
+                href="https://www.facebook.com/groups/2637132626546045"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="../assets/icons/Facebook.svg" alt="Facebook Logo" />
+              </a>
+              <a
+                class="flex inline-block w-5 h-2 content-center items-center m-2"
+                href="https://www.instagram.com/comfeco/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="../assets/icons/Instagram.svg" alt="Instagram Logo" />
+              </a>
+              <a
+                class="flex inline-block w-5 h-2 content-center items-center m-2"
+                href="https://discord.com/invite/5Bb5yvzNPr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="../assets/icons/discord.svg" alt="Discord Logo" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="ml-8 mt-3 mb-3 mr-8 inline-block">
+          <h2
+            class="relative font-medium mb-1 border-b-2 border-black max-w-max border-primary"
+          >
+            Quick Links
+          </h2>
+          <ul>
+            <li class="flex mt-1">
+              <router-link to="/Terminos-y-Condiciones" target="_blank"
+                >Privacity Policy</router-link
+              >
+            </li>
+            <li class="flex mt-1">
+              <a href="/Terminos-y-condiciones" target="_blank">Terms & Conditions</a>
+            </li>
+            <li class="flex mt-1">
+              <a href="https://discord.com/invite/5Bb5yvzNPr" target="_blank"
+                >Contac Us</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="w-96">
+          <div class="inline-block">
+            <img
+              class="w-10 h-10 ml-8 inline-block"
+              src="../assets/images/Isotipo-brand.png"
+              alt="COMFECO Logo"
+            />
+            <p class="w-max-w ml-8 inline-block">Comunity Fest and Code</p>
+          </div>
+          <p class="ml-32 sm:ml-32 text-center inline-block">COMFECO &copy; 2020</p>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Footer'
-    }
+export default {
+  name: "Footer",
+};
 </script>
